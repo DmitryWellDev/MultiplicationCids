c6345092c91b2b66359bac59d4fc87b02259969f
DmitryWellDew <dewwell0001@gmail.com>
2021-01-05 17:34:52 +0300

    absolutely working version after correction of suppport

--- src/Components/Multiple2/Multiple2.tsx
+++ src/Components/Multiple2/Multiple2.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react'
+import React, {ChangeEvent, useEffect, useState} from 'react'
 import styles from './Multiple2.module.css'
 import {CheckingImage} from "../CheckingImage/CheckingImage";
 
@@ -6,58 +6,73 @@ import {CheckingImage} from "../CheckingImage/CheckingImage";
 
 export const Multiple2 = () => {
     let multiplier = 2
-    let s: Array<number> = []
-    for (let i: number = 1; i < 11; i++) {
-        s.push(i)
-    }
 
-    let [multipleResult, setMultipleResult] = useState<boolean | null>(null)
+    let [arrayOfTasks, setArrayOfTasks] = useState<Array<{mult: number, isWrong: boolean}>>([])
 
-    let [answersArr, setAnswersArr] = useState<Array<string>>([])
+    //let [answersArr, setAnswersArr] = useState<Array<string>>([])
 
-    let w: Array<string> = []
-    w[0] = '0'
+    let [rightImage, setRightImage] = useState<boolean>(false)
 
-    const getInpValue = (value: string) => {
-        if (value !== '1') {
-            w.push(value)
+    useEffect(() => {
+        let arrayOfTasks: Array<{mult: number, isWrong: boolean}> = []
+        for (let i: number = 1; i < 11; i++) {
+            arrayOfTasks.push({mult: i, isWrong: false})
+            setArrayOfTasks(arrayOfTasks)
         }
-        if (w.length == 7) {
-            setAnswersArr(w)
-        }
-        console.log(w);
-        /*if(
-            // w[0] === '' &&
-            // w[1] === '4' &&
-            // w[2] === '6' &&
-            // w[3] === '8' &&
-            // w[4] === '10' &&
-            // w[5] === '12' &&
-            // w[6] === '14' &&
-            // w[7] === '16' &&
-            // w[8] === '18' &&
-            // w[9] === '20'
-            //  w[0] === '4'
-        ) {
-            alert('you did it!')
-        }*/
-    }
+    }, [])
+
+    // let w: Array<string> = []
+    // w[0] = '0'
+    //
+    // const getInpValue = (value: string) => {
+    //     if (value !== '1') {
+    //         w.push(value)
+    //     }
+    //
+    //     if (w.length == 7) {
+    //         setAnswersArr(w)
+    //     }
+    //     if(
+    //         w[0] === '' &&
+    //         w[1] === '4' &&
+    //         w[2] === '6' &&
+    //         w[3] === '8' &&
+    //         w[4] === '10' &&
+    //         w[5] === '12' &&
+    //         w[6] === '14' &&
+    //         w[7] === '16' &&
+    //         w[8] === '18' &&
+    //         w[9] === '20',
+    //          w[0] === '4'
+    //     ) {
+    //         alert('you did it!')
+    // }
+
     return (
         <div className={styles.main}>
-            {s.map((el, index) => <div key={index}>{multiplier} * {el} = <input onChange={
-                (e: ChangeEvent<HTMLInputElement>) => {
-                    if (e.currentTarget.value === `${multiplier * el}` || e.currentTarget.value === '1') {
-                        setMultipleResult(multipleResult = true)
-                    } else {
-                        setMultipleResult(multipleResult = false)
-                    }
-                    getInpValue(e.currentTarget.value)
-                }} className={multipleResult ? `${styles.inputStyle} ${styles.positiveInp}` : `${styles.inputStyle} ${styles.negativeInp}`}
-            />
-            </div>)}
+            {arrayOfTasks.map((el, index) => {
+                return (
+                    <div key={index}>{multiplier} * {el.mult} = <input onChange={
+                        (e: ChangeEvent<HTMLInputElement>) => {
+
+                            if (e.currentTarget.value === `${multiplier * el.mult}`) {
+                                arrayOfTasks[index] = {...arrayOfTasks[index], isWrong: true}
+                                setArrayOfTasks([...arrayOfTasks])
+                                setRightImage(rightImage = true)
+                            }else {
+                                arrayOfTasks[index] = {...arrayOfTasks[index], isWrong: false}
+                                setArrayOfTasks([...arrayOfTasks])
+                                setRightImage(rightImage = false)
+                            }
+                        }
+                    } className={el.isWrong ? `${styles.inputStyle} ${styles.positiveInp}` : `${styles.inputStyle} ${styles.negativeInp}`}
+                    />
+                    </div>
+                )
+            })}
             {/*----------------------------------------------------------------------------------------------------*/}
             {/*image under the table*/}
-            <CheckingImage multipleResult={multipleResult}/>
+            <CheckingImage multipleResult={rightImage}/>
         </div>
     )
 }
